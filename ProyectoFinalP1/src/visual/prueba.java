911a8b809ea67bafd50465042ccb5611cabfa372
mike0 <mike0@DESKTOP-J4EL9VL>
2020-03-26 15:38:00 -0400

    ultima prueba

--- ProyectoFinalP1/src/visual/prueba.java
+++ ProyectoFinalP1/src/visual/prueba.java
@@ -5,12 +5,15 @@ public class prueba {
 
 	private String hola;
 	private int numero = 0;
+	private int numero1 = 0;
 	
 	
 	
-	 public prueba(String hola) {
+	 public prueba(String hola, int numero, int numero1) {
 			super();
 			this.hola = hola;
+			this.numero = numero;
+			this.setNumero1(numero1);
 		}
 
 	public String getHola() {
@@ -33,6 +36,14 @@ public class prueba {
 
 	public void setNumero(int numero) {
 		this.numero = numero;
+	}
+
+	public int getNumero1() {
+		return numero1;
+	}
+
+	public void setNumero1(int numero1) {
+		this.numero1 = numero1;
 	}
 	 
 
